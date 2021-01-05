72c02cf33e435097e6f9d9732a1783cba3050acb
morganhrynyk <morganhrynyk@gmail.com>
2021-01-05 16:29:06 -0500

    main working

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from wetlands_example import \
 # GLOBAL VARIABLES
 # path variables
 WORKING_DIR_PATH = Path.cwd()  # current working directory ie. where the script lives
-RESULTS_DIR_PATH = WORKING_DIR_PATH.parents[
-                       1] / 'results_directory'  # root directory --> where GLPI wetlands data is stored on computer
-
+print(WORKING_DIR_PATH)
+RESULTS_DIR_PATH = WORKING_DIR_PATH.parent / 'results_directory'  # root directory --> where GLPI wetlands data is stored on computer
+print(RESULTS_DIR_PATH)
 # input variables
 GDB_NAMES = ['enney', 'meeny', 'miney', 'mo']
 
